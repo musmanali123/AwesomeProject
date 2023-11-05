8848fd33901fdfd2ecd538781c804059e2e962cb
Mohammad Usman Ali <usman232429@gmail.com>
2023-11-06 00:29:31 +0500

    main commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { Text } from "react-native";
+
+export const App  = () => {
+  return(
+    <>
+    <Text>Login Page</Text>
+    <Text>SignUP Page</Text>
+    <Text>Copy just same the Mindvally app and superior one</Text>
+    <Text>login have two btn (google login & login)</Text>
+    <Text>Signup constins dropdown with two value student and user </Text>
+</>
+  )
+}
+
+
+
+export default App;
+
